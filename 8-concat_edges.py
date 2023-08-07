8a8f0b8b13336f34c8716dbaf21a18daab7e2b8c
RajulaBonum <rajulabonum@gmail.com>
2023-08-07 18:09:47 +0300

    Better each day

---
+++ 8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is and interprated, interactive, object-oriented programming\ language that combines remarkable power with very clear syntax"
+str = str[40:67] + str[108:114] + str[:6]
+print(str)
