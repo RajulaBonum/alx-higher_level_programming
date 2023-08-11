e795bd385a2963346eee014def9d4e7c3cf176fe
RajulaBonum <rajulabonum@gmail.com>
2023-08-11 23:25:54 +0300

    block execute on import

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-args = (sys.argv[1:])
-result = 0
-i = len(args)
-if i >= 1:
-    for i in range(len(args)):
-        result += int(args[i])
-    print("{}".format(result))
-else:
-    print(0)
+if __name__ == '__main__':
+    import sys
+    args = sys.argv[1:]
+    result = 0
+    i = len(args)
+    if i >= 1:
+        for i in range(len(args)):
+            result += int(args[i])
+        print("{}".format(result))
+    else:
+        print(0)
