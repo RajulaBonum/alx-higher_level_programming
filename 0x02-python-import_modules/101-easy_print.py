991732abdfb99186e503ccb19e6c0e680ff0fd83
RajulaBonum <rajulabonum@gmail.com>
2023-08-11 23:56:11 +0300

    exec

---
+++ 0x02-python-import_modules/101-easy_print.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+exec(chr(112)+chr(117)+chr(116)+chr(115)+chr(40)+chr(34)+chr(112)+chr(121)+chr(116)+chr(104)+chr(111)+chr(110)+chr(105)+chr(115)+chr(99)+chr(111)+chr(111)+chr(108)+chr(44)+chr(32)+chr(102)+chr(111)+chr(108)+chr(108)+chr(111)+chr(119)+chr(101)+chr(100)+chr(98)+chr(121)+chr(32)+chr(97)+chr(32)+chr(110)+chr(101)+chr(119)+chr(32)+chr(108)+chr(105)+chr(110)+chr(101)+chr(44)+chr(41)+chr(10))
