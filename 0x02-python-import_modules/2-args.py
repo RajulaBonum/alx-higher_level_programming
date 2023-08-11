46795549a9368d9e1112c382b5c657da52f50525
RajulaBonum <rajulabonum@gmail.com>
2023-08-11 20:20:49 +0300

    fixing

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-import sys
-name_script = sys.argv[0]
-argv = sys.argv[1:]
-i = len(argv)
-if i < 1:
-    print("0 arguments")
-elif i == 1:
-    print("1 argument:")
-    print("1: {}".format(argv[0]))
-else:
-    print("{} arguments:".format(len(argv)))
-    for j in range(len(argv)):
-        print("{}: {}".format(j+1, argv[j]))
+if __name__=='__main__':
+    import sys
+    name_script = sys.argv[0]
+    argv = sys.argv[1:]
+    i = len(argv)
+    if i < 1:
+        print("0 arguments")
+    elif i == 1:
+        print("1 argument:")
+        print("1: {}".format(argv[0]))
+    else:
+        print("{} arguments:".format(len(argv)))
+        for j in range(len(argv)):
+            print("{}: {}".format(j+1, argv[j]))
 
