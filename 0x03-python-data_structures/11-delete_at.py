a8050a7f154e2e8fa4f34ba9c250070819bcfb82
RajulaBonum <rajulabonum@gmail.com>
2023-08-15 19:54:28 +0300

    used loops

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,7 +3,14 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
     new = []
-    for i, j in my_list:
+    i = 0
+    while i < len(my_list):
         if i != idx:
-            new.append(j)
+            new.append(my_list[i])
+        i+= 1
+    my_list[:] = []
+    i = 0
+    while i < len(new):
+        my_list.append(new[i])
+        i+= 1
     return new
