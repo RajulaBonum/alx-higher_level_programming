7fdf2e31650c43c5e115d147347531cca021ccd3
RajulaBonum <rajulabonum@gmail.com>
2023-09-12 01:00:42 +0300

    inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """function to get the attributes and methods of an object using dir()"""
 
-def lookup(object):
-    return [att for att in dir(att) if callable(getatt(object, att))]
+
+def lookup(obj):
+    """returns a list of objects"""
+    return dir(obj)
