8c54584733caae911708162840e0f2bbebb07f09
RajulaBonum <rajulabonum@gmail.com>
2023-12-14 09:11:44 -0800

    more on sql

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- 8-cities_of_california_subquery
+-- lists all cities of Carlifornia in the database hbtn_0d_usa.
+
+SELECT id, name
+FROM cities 
+WHERE state_id = 
+	(SELECT id FROM states
+	 WHERE name = 'California')
+ORDER BY id ASC;
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- 9-cities_by_state_join
+-- list all cities in the database hbtn_0d_usa
+-- record is in the form cities.id cities.name states.name
+
+SELECT c.id, c.name, s.name
+FROM cities c, states s
+WHERE c.state_id = s.id
+ORDER BY c.id;
